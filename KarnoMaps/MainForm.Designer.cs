46e73429f35dbdfc2acc9150ea552c35839aeabb
Xambey <Xambey@yandex.ru>
2016-09-16 23:32:42 +0300

    almost made fillTable(no)

--- KarnoMaps/MainForm.Designer.cs
+++ KarnoMaps/MainForm.Designer.cs
@@ -64,13 +64,15 @@
             // splitContainer1.Panel1
             // 
             this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
-            this.splitContainer1.Size = new System.Drawing.Size(1325, 645);
+            this.splitContainer1.Size = new System.Drawing.Size(1325, 721);
             this.splitContainer1.SplitterDistance = 583;
             this.splitContainer1.TabIndex = 0;
             // 
             // splitContainer2
             // 
+            this.splitContainer2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
             this.splitContainer2.Location = new System.Drawing.Point(0, 0);
             this.splitContainer2.Name = "splitContainer2";
             this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
@@ -82,15 +84,20 @@
             // splitContainer2.Panel2
             // 
             this.splitContainer2.Panel2.Controls.Add(this.textBox1);
-            this.splitContainer2.Size = new System.Drawing.Size(583, 645);
-            this.splitContainer2.SplitterDistance = 510;
+            this.splitContainer2.Panel2MinSize = 10;
+            this.splitContainer2.Size = new System.Drawing.Size(583, 721);
+            this.splitContainer2.SplitterDistance = 600;
             this.splitContainer2.TabIndex = 0;
             // 
             // dataGridView1
             // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(52)))));
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleVertical;
             this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
             dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
@@ -121,7 +128,6 @@
             dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.EnableHeadersVisualStyles = false;
             this.dataGridView1.GridColor = System.Drawing.Color.DarkSeaGreen;
             this.dataGridView1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
@@ -140,7 +146,7 @@
             this.dataGridView1.RowHeadersWidth = 30;
             this.dataGridView1.RowTemplate.Height = 28;
             this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
-            this.dataGridView1.Size = new System.Drawing.Size(583, 510);
+            this.dataGridView1.Size = new System.Drawing.Size(581, 598);
             this.dataGridView1.TabIndex = 1;
             // 
             // dataGridViewTextBoxColumn1
@@ -214,22 +220,25 @@
             // 
             // textBox1
             // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(3, 2);
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(580, 129);
-            this.textBox1.TabIndex = 0;
+            this.textBox1.Size = new System.Drawing.Size(581, 116);
+            this.textBox1.TabIndex = 1;
             this.textBox1.Text = "Введите ваш HEX номер \r\nнажмите ENTER после ввода";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseClick);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(52)))));
-            this.ClientSize = new System.Drawing.Size(1325, 645);
+            this.ClientSize = new System.Drawing.Size(1325, 721);
             this.Controls.Add(this.splitContainer1);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -251,7 +260,6 @@
 
         private System.Windows.Forms.SplitContainer splitContainer2;
         private System.Windows.Forms.SplitContainer splitContainer1;
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
@@ -262,6 +270,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn8;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn9;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn10;
+        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.TextBox textBox1;
     }
 }
--- KarnoMaps/MainForm.cs
+++ KarnoMaps/MainForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,18 +21,71 @@ namespace KarnoMaps
 
         private void initialTable()
         {
-            dataGridView1.Rows.Add(14);
+            dataGridView1.Rows.Add(15);
         }
 
-        private void fillTable()
+        private void incrementByte(ref BitArray n, int i)
         {
+            if (i >= 15) return;
+            for (int j = 0; i < n.Count; i++)
+            {
+                if (n[j])
+                    n[j] = false;
+                else
+                    return;
+            }
+        }
+
+        private void fillTable(string s)
+        {
+            BitArray n = new BitArray(4, false);
+            string bin = "";
+            s = new string(s.Reverse<char>().ToArray());
+            int k = 0;
+            
+
             for (int i = 0; i < dataGridView1.Rows.Count; i++)
             {
                 for (int j = 0; j < dataGridView1.Columns.Count; j++)
                 {
-
+                    if(j == 0)
+                    {
+                        dataGridView1.Rows[i].Cells[j].Value = Convert.ToString(i, 16);
+                    }
+                    else if(j < 5)
+                    {
+                        dataGridView1.Rows[i].Cells[j].Value = n[k] ? "1" : "0";
+                        k++;
+                    }
+                    else if(j == 5)
+                    {
+                        bin = Convert.ToString(Convert.ToInt32(s[i].ToString(), 16), 2);
+                        while(bin.Count() - 4 != 0)
+                        {
+                            bin = "0" + bin;
+                        }
+                        k = 0;
+                        dataGridView1.Rows[i].Cells[j].Value = s[i];
+                    }
+                    else
+                    {
+                        dataGridView1.Rows[i].Cells[j].Value = bin[k];
+                        k++;
+                    }
                 }
+                k = 0;
+                incrementByte(ref n, i);
             }
+        }
+
+        private void textBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            fillTable(textBox1.Text);
         }
     }
 }
